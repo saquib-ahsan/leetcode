82191cac5881f11545a74e20a76a31f0b265a83e
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-05-01 10:50:08 +0530

    Create 1936-Add-Minimum-Number-of-Rungs.cpp

---
+++ all-problems/1936-Add-Minimum-Number-of-Rungs.cpp
@@ -0,0 +1,20 @@
+// problem statement
+https://leetcode.com/problems/add-minimum-number-of-rungs/
+
+#define sz(x) (int)(x).size()
+
+class Solution {
+public:
+    int addRungs(vector<int>& rungs, int dist) {
+        int ans = 0, prev = 0;
+        for(int i = 0; i < sz(rungs); ++i) {
+            if(rungs[i] - prev > dist) {
+                ans += (rungs[i] - prev - 1) / dist;
+            }
+            
+            prev = rungs[i];
+        }
+        
+        return ans;
+    }
+};
