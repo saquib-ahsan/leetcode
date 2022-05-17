5a6eec923b35b71b863d9d1f152a9aa09eeba25e
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-05-17 11:28:17 +0530

    Update 119-Pascal's-Triangle-II.cpp

--- all-problems/119-Pascal's-Triangle-II.cpp
+++ all-problems/119-Pascal's-Triangle-II.cpp
@@ -21,3 +21,29 @@ public:
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // RECURSIVE IMPLEMENTATION
+class Solution {
+private:
+    void solve(vector<int> &ans, int i, int row_index) {
+        if(i > row_index) return;
+        
+        int n = (int)(ans).size();
+        int prev = 0;
+        for(int j = 0; j < n; ++j) {
+            int tmp = ans[j];
+            ans[j] = prev + ans[j];
+            prev = tmp;
+        }
+        ans.push_back(1);
+        
+        solve(ans, i+1, row_index);
+    }
+    
+public:
+    vector<int> getRow(int row_index) {
+        vector<int> ans;
+        int i = 0;
+        
+        solve(ans, i, row_index);
+        return ans;
+    }
+};
