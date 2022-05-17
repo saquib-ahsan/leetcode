db34e3e17ce4e67d24641b4c6bec6ae4ed0a19e0
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-05-17 11:51:48 +0530

    Create Climbing-Stairs.cpp

---
+++ explore card = recursion-I/Climbing-Stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int solve(int n, unordered_map<int, int> &dp) {
+        if(n < 0) return 0;
+        if(dp[n]) return dp[n];
+        
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+    
+public:
+    int climbStairs(int n) {
+        unordered_map<int, int> dp;
+        dp[0] = 1;
+        
+        return solve(n, dp);
+    }
+};
