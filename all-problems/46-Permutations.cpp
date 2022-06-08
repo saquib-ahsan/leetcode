07f2c3997856c8f7af8442a94a3915c35a07e898
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-06-08 11:03:42 +0530

    Update 46-Permutations.cpp

--- all-problems/46-Permutations.cpp
+++ all-problems/46-Permutations.cpp
@@ -15,3 +15,30 @@ public:
 
 
 // BACKTRACKING
+#define vt vector
+#define pb push_back
+#define sz(x) (int)(x).size()
+
+class Solution {
+    void solve(vt<int> &nums, int i, vt<vt<int>> &ans) {
+        if(i == sz(nums)-1) {
+            ans.pb(nums);
+            return;
+        }
+        
+        for(int j = i; j < sz(nums); ++j) {
+            swap(nums[i], nums[j]);
+            
+            solve(nums, i+1, ans);
+            
+            swap(nums[i], nums[j]);
+        }
+    }
+    
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vt<vt<int>> ans;
+        solve(nums, 0, ans);
+        return ans;
+    }
+};
