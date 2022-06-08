6fb4bb177aff98e6e0257ba0ecb42cec30597613
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-06-08 12:10:38 +0530

    Update 47-Permutations-II.cpp

--- all-problems/47-Permutations-II.cpp
+++ all-problems/47-Permutations-II.cpp
@@ -21,4 +21,35 @@ public:
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
-BACKTRACKING
+// BACKTRACKING - I THINK, WE CAN MORE OPTIMIZED IN TERMS OF SPACE
+#define vt vector
+#define pb push_back
+#define sz(x) (int)(x).size()
+
+class Solution {
+private:
+    void solve(vt<int> &nums, int i, map<vt<int>, bool> &kv) {
+        if(i == sz(nums)-1) {
+            kv[nums] = true;
+            return;
+        }
+        
+        for(int j = i; j < sz(nums); ++j) {
+            if((i != j) && (nums[i] == nums[j])) continue;
+            
+            swap(nums[i], nums[j]);
+            solve(nums, i+1, kv);
+            swap(nums[i], nums[j]);
+        }
+    }
+    
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        map<vt<int>, bool> kv;
+        solve(nums, 0, kv);
+        
+        vt<vt<int>> ans;
+        for(auto &p: kv) ans.pb(p.first);
+        return ans;
+    }
+};
