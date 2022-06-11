7e8fb7dc7d3a1121f5234cd2ece3bb3ce2872f54
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-06-11 15:44:23 +0530

    Update 1006-Clumsy-Factorial.cpp

--- all-problems/1006-Clumsy-Factorial.cpp
+++ all-problems/1006-Clumsy-Factorial.cpp
@@ -5,111 +5,106 @@ https://leetcode.com/problems/clumsy-factorial/
 
 /*
 APPROACH:
-    - MAKE AN EXPRESSION(INFIX) FROM THE NUMBER
-    - CONVERT THE INFIX NOATION TO POSTFIX NOTATION
-    - EVALUATE THE POSTFIX NOTATION
+    - Generate an infix expression from the number
+    - Convert infix notation to postfix notation
+    - Evaluate the postfix notation
 */
 
 #define vt vector
-#define sz(x) (int)(x).size()
 #define pb push_back
-#define repe(a, x) for(auto &a: x)
+#define sz(x) (int)(x).size()
 
 class Solution {
 private:
     bool is_operand(string s) {
         return !((s == "/") || (s == "*") || (s == "+") || (s == "-"));
     }
-
-    // infix to postfix conversion
-    vt<string> if_to_pf(const vt<string> &vs) {
-        vt<string> expr;
+    
+    vt<string> num_to_infix(int n) {
+        bitset<4> bs(0);    // multiply, division, addition, subtraction
+        bs[0] = 1;
+        
+        vt<string> infix = {to_string(n)};
+        while(--n) {
+            if(bs[0]) {
+                infix.pb("*");
+                bs[0] = 0, bs[1] = 1;
+            } else if(bs[1]) {
+                infix.pb("/");
+                bs[1] = 0, bs[2] = 1;
+            } else if(bs[2]) {
+                infix.pb("+");
+                bs[2] = 0, bs[3] = 1;
+            } else if(bs[3]) {
+                infix.pb("-");
+                bs[3] = 0, bs[0] = 1;
+            }
+            
+            infix.pb(to_string(n));
+        }
+        
+        return infix;
+    }
+    
+    vt<string> num_to_postfix(int n) {
+        vt<string> infix = num_to_infix(n);
+        
+        vt<string> postfix;
         stack<string> stk;
-        repe(s, vs) {
-            if (is_operand(s)) {
-                expr.pb(s);
+        for(const auto &s: infix) {
+            if(is_operand(s)) {
+                postfix.pb(s);
             } else {
-                if (s == "/" || s == "*") {
-                    while (sz(stk) && (stk.top() == "/" || stk.top() == "*")) {
-                        expr.pb(stk.top());
+                if(s=="/" || s=="*") {
+                    while(sz(stk) && (stk.top()=="/" || stk.top()=="*")) {
+                        postfix.pb(stk.top());
                         stk.pop();
                     }
                 } else {
-                    while (sz(stk) && (stk.top() != "+" || stk.top() != "-")) {
-                        expr.pb(stk.top());
+                    while(sz(stk)) {
+                        postfix.pb(stk.top());
                         stk.pop();
                     }
                 }
-
+                
                 stk.push(s);
             }
-
         }
-        while (sz(stk)) {
-            expr.pb(stk.top());
+        while(sz(stk)) {
+            postfix.pb(stk.top());
             stk.pop();
         }
-
-        // repe(s, expr) cout << s;
-        // cout << nl;
-
-        return expr;
+        
+        return postfix;
     }
-
-    int eval_expr(const vt<string> &vs) {
-        vt<string> expr = if_to_pf(vs);
+    
+    int evaluate(int n) {
+        vt<string> postfix = num_to_postfix(n);
         stack<int> stk;
-        repe(s, expr) {
-            if (is_operand(s)) {
+        
+        for(const auto &s: postfix) {
+            if(is_operand(s)) {
                 stk.push(stoi(s));
             } else {
                 int b = stk.top(); stk.pop();
                 int a = stk.top(); stk.pop();
-
-                int res = 0;
-                if (s == "/") res = a / b;
-                else if (s == "*") res = a * b;
-                else if (s == "+") res = a + b;
-                else res = a - b;
-
-                stk.push(res);
+                
+                int tmp = 0;
+                if(s == "/") tmp = a / b;
+                else if(s == "*") tmp = a * b;
+                else if(s == "+") tmp = a + b;
+                else tmp = a - b;
+                
+                stk.push(tmp);
             }
         }
-
+        
         return stk.top();
     }
     
 public:
     int clumsy(int n) {
-        bitset<4> bs(0);    // multiply, division, addition, subtraction
-        bs[0] = 1;
-        vt<string> expr;
-        expr.pb(to_string(n));
-        --n;
-        while (n) {
-            if (bs[0]) {
-                expr.pb("*");
-                expr.pb(to_string(n));
-                bs[0] = 0; bs[1] = 1;
-            } else if (bs[1]) {
-                expr.pb("/");
-                expr.pb(to_string(n));
-                bs[1] = 0; bs[2] = 1;
-            } else if (bs[2]) {
-                expr.pb("+");
-                expr.pb(to_string(n));
-                bs[2] = 0; bs[3] = 1;
-            } else {
-                expr.pb("-");
-                expr.pb(to_string(n));
-                bs[3] = 0; bs[0] = 1;
-            }
-
-            --n;
-        }
-        
-        return eval_expr(expr);
+        int ans = evaluate(n);
+        return ans;
     }
 };
-
-// P.S.: Code became longer than thought, so i think optimization is needed!!
