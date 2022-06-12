2f7058a56f87bfa8e68fa5bba3583575bd31b10e
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-06-12 21:16:53 +0530

    Update 36-Valid-Sudoku.cpp

--- all-problems/36-Valid-Sudoku.cpp
+++ all-problems/36-Valid-Sudoku.cpp
@@ -54,3 +54,53 @@ public:
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // RECURSION IMPLEMENTATION METHOD
+#define vt vector
+
+static const int N = 9;
+static const int n = 3;
+
+class Solution {
+private:
+    bool num_placed_correctly(const vt<vt<char>> &board, int x, int y, char num) {
+        // check row and column
+        for(int k = 0; k < N; ++k) {
+            if(board[x][k] == num || board[k][y] == num) return false;
+        }
+        
+        // check sub-grid
+        int sx = (x / n) * n;   // x-coordinate of subgrid
+        int sy = (y / n) * n;   // y-coordinate of subgrid
+        for(int i = sx; i < sx+n; ++i) {
+            for(int j = sy; j < sy+n; ++j) {
+                if(board[i][j] == num) return false;
+            }
+        }
+        
+        // hurray, num placed correctly
+        return true;
+    }
+    
+    bool solve(vt<vt<char>> &board, int i, int j) {
+        if(i >= N) return true;
+        
+        // check if we crossed last column
+        if(j == N) return solve(board, i+1, 0);
+        
+        // check if current cell is empty
+        if(board[i][j] == '.') return solve(board, i, j+1);
+        
+        char tmp = board[i][j];
+        board[i][j] = '.';
+        bool placed_correctly = num_placed_correctly(board, i, j, tmp);
+        board[i][j] = tmp;
+        
+        bool check_next_cell = solve(board, i, j+1);
+        return (placed_correctly && check_next_cell);
+    }
+    
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool ans = solve(board, 0, 0);
+        return ans;
+    }
+};
