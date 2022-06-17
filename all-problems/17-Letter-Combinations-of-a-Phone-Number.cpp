f08af4e3bc0d1427d5957c40c6871d619bc94177
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-06-17 20:01:15 +0530

    Update 17-Letter-Combinations-of-a-Phone-Number.cpp

--- all-problems/17-Letter-Combinations-of-a-Phone-Number.cpp
+++ all-problems/17-Letter-Combinations-of-a-Phone-Number.cpp
@@ -1,41 +1,39 @@
 #define vt vector
-#define pb push_back
 #define sz(x) (int)(x).size()
+#define pb push_back
 
-static unordered_map<char, string> kv;
-static vt<string> ans;
+static unordered_map<char, string> kv = {
+    {'2', "abc"},
+    {'3', "def"},
+    {'4', "ghi"},
+    {'5', "jkl"},
+    {'6', "mno"},
+    {'7', "pqrs"},
+    {'8', "tuv"},
+    {'9', "wxyz"}
+};
 
 class Solution {
-    void solve(const string &d, int i, string &s) {
-        if(i >= sz(d)) {
-            if(sz(s)) ans.pb(s);
+private:
+    void solve(const string &digits, int i, string &s, vt<string> &ans) {
+        if(i >= sz(digits) && sz(s)) {
+            ans.pb(s);
             return;
         }
         
-        string tmp = kv[d[i]];
-        for(const auto &c: tmp) {
+        string tmp = kv[digits[i]];
+        for(auto &c: tmp) {
             s[i] = c;
-            solve(d, i+1, s);
+            solve(digits, i+1, s, ans);
         }
     }
     
 public:
     vector<string> letterCombinations(string digits) {
-        kv = {
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"}
-        };
-        
-        ans.clear();
         string s(sz(digits), 'x');
+        vt<string> ans;
         
-        solve(digits, 0, s);
+        solve(digits, 0, s, ans);
         return ans;
     }
 };
