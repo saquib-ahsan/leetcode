a2efe42052959ec3a3c1eda3a5190a2905584e02
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-06-22 10:49:25 +0530

    Create 24-Swap-Nodes-in-Pairs.cpp

---
+++ data-structures/linked-list/24-Swap-Nodes-in-Pairs.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head;
+        
+        ListNode *a = head, *b = head->next;
+        head = head->next->next;
+        b->next = a;
+        a->next = nullptr;
+        
+        ListNode *ans = b, *last = a;
+        while(head && head->next) {
+            a = head;
+            b = head->next;
+            head = head->next->next;
+            
+            b->next = a;
+            a->next = nullptr;
+            
+            last->next = b;
+            last = a;
+        }
+        
+        if(head) last->next = head;
+        return ans;
+    }
+};
