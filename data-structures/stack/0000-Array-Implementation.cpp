3617b5f297956f09bee3a28ba0fc792b5e78fbb4
Saquib Ahsan <85388160+saquib-ahsan@users.noreply.github.com>
2022-07-14 17:38:32 +0530

    Create 0000-Array-Implementation.cpp

---
+++ data-structures/stack/0000-Array-Implementation.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 101;
+
+class Stack {
+private:
+	int arr[N];
+	int top;
+
+public:
+	void Push(int x);
+	void Pop();
+	int Top();
+	bool isEmpty();
+
+	// constructor
+	Stack() {
+		top = -1;
+	}
+};
+
+void Stack::Push(int x) {
+	if(top == N-1) return;
+
+	++top;
+	arr[top] = x;
+}
+
+void Stack::Pop() {
+	if(isEmpty()) return;
+	--top;
+}
+
+int Stack::Top() {
+	if(isEmpty()) return INT_MIN;
+	return arr[top];
+}
+
+bool Stack::isEmpty() {
+	return (top == -1);
+}
+
+
+int main() {
+	Stack s;
+	s.Push(1);
+	s.Push(2);
+	s.Push(3);
+
+	cout << s.Top() << endl;
+
+	s.Pop();
+	cout << s.Top() << endl;
+
+	return 0;
+}
+
+// P.S.: We can increase the array size in case we hit an overflow by using dynamic array. Check if it is an overflow, create
+// a new array (generally doubled size of previous one) and copy the data from older array.
